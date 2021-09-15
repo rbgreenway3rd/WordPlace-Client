34a388bd3ffebbfa43a6563a65a3a24de98702ae
rbgreenway3rd <3rdgreenway@gmail.com>
2021-09-15 16:41:24 -0500

    Favorited words files constructed and added to myprofile. Bug: all words render to dom. need only user-specific data to render.

---
+++ src/components/FavoritedWords/FavoritedWordsList.js
@@ -0,0 +1,64 @@
+import React, { useState, useContext, useEffect } from "react";
+import { FavoritedWordsContext } from "./FavoritedWordsProvider";
+// import FavoritedWords from "./FavoritedWords";
+import "./FavoritedWords.css";
+import { useHistory } from "react-router-dom";
+
+export const FavoritedWordsList = () => {
+  const { favoritedwords, getFavoritedWords } = useContext(
+    FavoritedWordsContext
+  );
+  const history = useHistory();
+  // const [filteredfavoritedwords, setFiltered] = useState([])
+
+  // Initialization effect hook -> Go get favoritedword data
+  useEffect(() => {
+    getFavoritedWords();
+  }, []);
+
+  // useEffect(() => {
+  //     const matchingfavoritedwords = favoritedwords.filter(favoritedword => favoritedword.name.toLowerCase().includes(searchTerms.toLowerCase()))
+  //     setFiltered(matchingfavoritedwords)
+  // }, [searchTerms])
+
+  // useEffect(() => {
+  //     setFiltered(favoritedwords)
+  // }, [favoritedwords])
+
+  return (
+    <section style={{ marginTop: "2rem" }} className="favoritedwords_list">
+      {/* <button
+        onClick={() => history.push("/favoritedwords/create")}
+        className="addFavoritedWords__button"
+      >
+        Define a New Word
+      </button> */}
+      <div className="favoritedwords">
+        {favoritedwords.map((favoritedword) => (
+          <div
+            className="favoritedword"
+            id={`favoritedword--${favoritedword.id}`}
+            key={favoritedword.id}
+          >
+            <div className="favoritedword__word">
+              {" "}
+              Word: {favoritedword.word}
+            </div>
+            <div className="favoritedword__definition">
+              {" "}
+              Definition: {favoritedword.definition}
+            </div>
+            <div className="favoritedword__partOfSpeech">
+              {" "}
+              Part of speech: {favoritedword.partOfSpeech}{" "}
+            </div>
+            <a className="favoritedword__link" href={favoritedword.link}>
+              {" "}
+              Link
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
---
+++ src/components/FavoritedWords/FavoritedWordsProvider.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+export const FavoritedWordsContext = React.createContext();
+
+export const FavoritedWordsProvider = (props) => {
+  const [favoritedwords, setFavoritedWords] = useState([]);
+
+  const getFavoritedWords = () => {
+    return fetch("http://localhost:8000/favoritedwords", {
+      headers: {
+        Authorization: `Token ${localStorage.getItem("wordplace_token")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(setFavoritedWords);
+  };
+
+  const getFavoritedWordsById = (id) => {
+    return fetch(`http://localhost:8000/favoritedwords/${id}?_expand=users`, {
+      headers: {
+        Authorization: `Token ${localStorage.getItem("wordplace_token")}`,
+      },
+    }).then((res) => res.json());
+  };
+  const addFavoritedWords = (favoritedwords) => {
+    return fetch("http://localhost:8000/favoritedwords", {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("wordplace_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(favoritedwords),
+    }).then(getFavoritedWords);
+  };
+
+  const updateFavoritedWords = (favoritedwords) => {
+    return fetch(`http://localhost:8000/favoritedwords/${favoritedwords.id}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Token ${localStorage.getItem("wordplace_token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(favoritedwords),
+    }).then(getFavoritedWords);
+  };
+
+  const deleteFavoritedWords = (favoritedwordsId) => {
+    return fetch(`http://localhost:8000/favoritedwords/${favoritedwordsId}`, {
+      method: "DELETE",
+    }).then(getFavoritedWords);
+  };
+
+  return (
+    <FavoritedWordsContext.Provider
+      value={{
+        favoritedwords,
+        addFavoritedWords,
+        getFavoritedWords,
+        getFavoritedWordsById,
+        updateFavoritedWords,
+        deleteFavoritedWords,
+      }}
+    >
+      {props.children}
+    </FavoritedWordsContext.Provider>
+  );
+};
